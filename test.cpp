ec7da3725e1a141710797bbd2e56bf199b6c7649
Evginii <eol@braehler-systems.com>
2023-09-18 09:02:51 +0200

    fix

--- test.cpp
+++ test.cpp
@@ -1,4 +1,4 @@
-﻿// test.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// test.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
